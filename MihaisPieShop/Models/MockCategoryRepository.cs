7c8bea5e666ac7d2a45ff9f0f2f61afa53b8dafa
Mihai Muntean <mihai.muntean@gilead.com>
2021-01-18 14:12:46 +0200

    Add project files.

---
+++ MihaisPieShop/Models/MockCategoryRepository.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MihaisPieShop.Models
+{
+    public class MockCategoryRepository : ICategoryRepository
+    {
+        public IEnumerable<Category> AllCategories => new List<Category>
+        { 
+            new Category{CategoryId=1, CategoryName="Fruit pies", Description="All-fruit pie"},
+            new Category{CategoryId=1, CategoryName="Fruit pies", Description="Cheesy all the way"},
+            new Category{CategoryId=1, CategoryName="Fruit pies", Description="Seasonal pie"}
+        };
+
+    }
+}
