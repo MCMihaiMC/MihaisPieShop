0ba7f96f9585b276e4ab812950b4ea00a861ac6b
Mihai Muntean <muntean.mihai70@gmail.com>
2021-01-22 14:11:18 +0200

    Code cleaning - view component

--- MihaisPieShop/Components/ShoppingCartSummary.cs
+++ MihaisPieShop/Components/ShoppingCartSummary.cs
@@ -1,10 +1,6 @@
 ﻿using Microsoft.AspNetCore.Mvc;
 using MihaisPieShop.Models;
 using MihaisPieShop.ViewModels;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
 
 namespace MihaisPieShop.Components
 {
